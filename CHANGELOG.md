20fd96c8bb42907c2861721361fda6800ad85323
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-03-12 13:21:03 +0100

    Version 1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## [0.0.1] - March 11, 2019
 
 * Initial version
+
+## [1.0.0] - March 12, 2020
+
+* Change in some function signatures
--- lib/basic_clean_arch.dart
+++ lib/basic_clean_arch.dart
@@ -3,8 +3,11 @@ library basic_clean_arch;
 /// A Displayer is used to display the result of a use case that changes the
 /// displayed view.
 abstract class Displayer<T> {
-  /// Displays data from a ViewModel or an Error contained in [result].
-  void display({Future<T> result});
+  /// Displays the [result] of a successful use case.
+  void display({T result, resultCode = 0});
+
+  /// Displays an [error] after an unsuccessful use case.
+  void displayError(Exception error);
 }
 
 /// A Presenter is used to get a ViewModel from Entities or other Models.
@@ -19,5 +22,5 @@ abstract class UseCase<RequestType,Model,ViewModel> {
   /// The [Presenter] to use.
   Presenter<Model,ViewModel> presenter;
   /// Executes the UseCase for the given [request].
-  void execute({RequestType request,Displayer displayer});
+  void execute({RequestType request, Displayer displayer, resultCode = 0});
 }
